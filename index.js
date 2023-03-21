2891792f50b5a9ac8aee67afef861aaa1caf4364
Sarah Jones <sarahrjones8@gmail.com>
2023-03-21 11:59:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function() {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function fareMultiplier(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(); 
+}
+
+function selectDifferentDrivers(arrayOfDrivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(); 
+}
+
+
